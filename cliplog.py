2f981c8d423ba8fca3071dd8f49feff975ac9a15
Rohit Tatu <r-tatu@users.noreply.github.com>
2017-11-18 19:26:54 +0530

    Added cliplog.py
    This program will take the address the of  the file as input and will save the clipboard to the file

---
+++ cliplog.py
@@ -0,0 +1,26 @@
+import pyperclip
+
+class cliplog:
+	def __init__(self,address):
+		self.clipboard = pyperclip.paste()
+		self.temp = ''
+		self.address = address
+
+	def log(self):
+		with open(self.address,'w+') as file:
+			while True:
+				try:
+					if self.clipboard != self.temp:
+						file.write(self.clipboard+'\n')
+						self.temp = self.clipboard
+						self.clipboard = pyperclip.paste()
+					else:
+						self.clipboard = pyperclip.paste()
+				except KeyboardInterrupt:
+					break
+		print("\nThe log has been successfully saved in {}".format(self.address))
+		return 0
+
+c = input('Path to file: ')
+obj = cliplog(c)
+obj.log()
